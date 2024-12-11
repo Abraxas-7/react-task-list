95f456e424a964bc82d494491e0c112782dd0bed
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-12-11 16:20:09 +0100

    app + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import tasks from "./models/tasks.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const tasksUndone = tasks.filter(
+    (task) => task.state === "backlog" || task.state === "in_progress"
+  );
+
+  const tasksDone = tasks.filter((task) => task.state === "completed");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Task Manager</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>Current Tasks ({tasksUndone.length})</h2>
+
+          <ul>
+            {tasksUndone.map((task) => (
+              <li>
+                <h4>{task.title}</h4>
+                <p>Priority: {task.priority}</p>
+                <p>Estimated Time: {task.estimatedTime}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <hr />
+
+        <section>
+          <h2>Completed Tasks</h2>
+
+          <ul>
+            {tasksDone.map((task) => (
+              <li>
+                <h4>{task.title}</h4>
+                <p>Priority: {task.priority}</p>
+                <p>Estimated Time: {task.estimatedTime}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
